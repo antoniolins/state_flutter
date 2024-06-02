75b37e16b2f3320b462091fbf24c8a6587286011
antoniolins <antoniolins@uol.com.br>
2024-06-02 17:54:55 -0300

    Bloc Provider

--- lib/main.dart
+++ lib/main.dart
@@ -13,16 +13,11 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
@@ -88,8 +83,7 @@ class _MyHomePageState extends State<MyHomePage> {
           ),
               FloatingActionButton(
             onPressed: () {
- //             counterBloc.add(CounterDecrementEvent());
-             context.read<CounterBloc>().add(CounterDecrementEvent());
+              context.read<CounterBloc>().add(CounterDecrementEvent());
             },
             tooltip: 'Decrement',
             child: const Icon(Icons.minimize),
